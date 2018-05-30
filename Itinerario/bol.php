a219b391b220bba086518ccc47bb32049d9181f8
Krico16 <bash_stone_u@hotmail.com>
2018-05-30 00:52:35 -0500

    Proceso 1
    Signed-off-by: Krico16 <bash_stone_u@hotmail.com>

---
+++ Itinerario/bol.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="">
+<?php
+    include("../utils.php");
+    include("../conn.php");
+    ?>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>GOTTA GO FAST Inc</title>
+    <link rel="icon" href="https://cdn.discordapp.com/attachments/439961746157993984/450468016933699594/logo_circulo.png">
+    <?php imports(); ?>
+</head>
+
+<body>
+   <?php
+    crearbanner();
+    crearnavbar("../principal.php" , "../Clients/ver.php","../Employees/ver.php","","../Rutas/ver.php");
+    
+    
+    
+    $sql= "SELECT * FROM Boleto";
+    
+    $result = $conn->query($sql);
+    
+    //IdItinerario IdPromocion IdRuta IdEncomienda IdMaleta IdVehiculo
+    //ALTER TABLE `Itinerario` CHANGE `DetalleEncomienda` REFERENCES DetalleEncomieda(IdDetalle);
+    if($result->num_rows > 0){
+        echo "
+           <table>
+                <tr>
+                   <th>ID</th>
+                   <th>Ruta</th>
+                   <th>Precio</th>
+                </tr>";
+        
+        while($row = $result->fetch_assoc()){
+            echo "
+                    <tr>
+                        <td> " . $row["IDBoleto"] ." </td>
+                        <td> " . $row["IdRuta"] . " </td>
+                        <td> S/." . $row["Precio"] . " </td>"
+/*                        <td> " . $row["Ciudadd"] . " </td>
+                        <td> " . $row["Placa"] . " </td>*/
+                    ."</tr>"; 
+            
+        }
+        echo "</table>";
+    }else{
+        noresults();
+    }
+    $conn->close();
+    ?>        
+    
+</body>
+</html>
---
+++ principal.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="Es">
+<?php include ('utils.php'); ?>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css">
+    <title>GOTTA GO FAST Inc</title>
+    <link rel="icon" href="https://cdn.discordapp.com/attachments/439961746157993984/450468016933699594/logo_circulo.png">
+    <?php imports(); ?>
+</head>
+
+<body style="background-color:#aad1ef">
+    <?php
+    crearbanner();
+    crearnavbar("#","Clients/ver.php","Employees/ver.php","Itinerario/ver.php","Rutas/ver.php");
+    
+    ?>
+
+
+
+    <img src="https://cdn.discordapp.com/attachments/439961746157993984/450468016933699594/logo_circulo.png" width="100%" height="auto">
+
+
+</body>
+
+</html>
